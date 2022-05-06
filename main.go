7301689a26791cbe58c23b7961cf2b6e217909f4
Yami <evila@qq.com>
2022-05-07 00:55:51 +0800

    集成 gin

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello Yami!")
+	r := gin.New()
+
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"hello": "world",
+		})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		// 获取标头信息的 Accept 信息
+		acceptString := ctx.Request.Header.Get("Accept")
+
+		if strings.Contains(acceptString, "text/html") {
+			// 如果是 HTML 的话
+			ctx.String(http.StatusNotFound, "Page not found")
+		} else {
+			// 默认返回 JSON
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+
+	})
+
+	r.Run(":8000")
 }
