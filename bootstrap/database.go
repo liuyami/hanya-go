496f329dfe763f273103bb2b721bf6da23f4dad8
亚米 <evila@qq.com>
2022-05-07 15:38:04 +0800

    数据库请求日志

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 	"hanya-go/pkg/config"
 	"hanya-go/pkg/database"
+	"hanya-go/pkg/logger"
 	"time"
 )
 
@@ -34,7 +34,7 @@ func SetupDB() {
 	}
 
 	// 连接数据库，并设置 GORM 的日志模式
-	database.Connect(dbConfig, logger.Default.LogMode(logger.Info))
+	database.Connect(dbConfig, logger.NewGormLogger())
 
 	// 设置最大连接数
 	database.SQLDB.SetMaxOpenConns(config.GetInt("database.mysql.max_open_connections"))
