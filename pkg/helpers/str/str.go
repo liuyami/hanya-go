21fba6890b0389dd1310ba18cf8c54330048773a
Yami <evila@qq.com>
2022-05-13 01:05:00 +0800

    参数格式化（str 包）

--- pkg/helpers/str/str.go
+++ pkg/helpers/str/str.go
@@ -2,6 +2,8 @@ package str
 
 import (
 	"fmt"
+	"github.com/gertd/go-pluralize"
+	"github.com/iancoleman/strcase"
 	"strings"
 	"unsafe"
 )
@@ -11,6 +13,32 @@ func SliceSplit(slice []string, separator string) string {
 	return strings.Replace(strings.Trim(fmt.Sprint(slice), "[]"), " ", separator, -1)
 }
 
+// ByteToString byte 转字符串
 func ByteToString(b []byte) string {
 	return *(*string)(unsafe.Pointer(&b))
+}
+
+// Plural 转为复数 user -> users
+func Plural(word string) string {
+	return pluralize.NewClient().Singular(word)
+}
+
+// Singular 转为单数 users -> user
+func Singular(word string) string {
+	return pluralize.NewClient().Singular(word)
+}
+
+// Snake 转为 snake_case，如 TopicComment -> topic_comment
+func Snake(s string) string {
+	return strcase.ToSnake(s)
+}
+
+// Camel 转为 CamelCase，如 topic_comment -> TopicComment
+func Camel(s string) string {
+	return strcase.ToCamel(s)
+}
+
+// LowerCamel 转为 lowerCamelCase，如 TopicComment -> topicComment
+func LowerCamel(s string) string {
+	return strcase.ToLowerCamel(s)
 }
