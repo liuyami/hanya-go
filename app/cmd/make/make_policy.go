5fb5f1855d9078d46920071328b3c53fe4f75a0b
Yami <evila@qq.com>
2022-05-17 00:36:48 +0800

    make policy 命令

--- app/cmd/make/make_policy.go
+++ app/cmd/make/make_policy.go
@@ -7,15 +7,16 @@ import (
 	"strings"
 )
 
-var CmdMakeAPI = &cobra.Command{
-	Use:   "api",
-	Short: "Create api controller，exmaple: make api auth/user",
-	Run:   runMakeAPI,
+var CmdMakePolicy = &cobra.Command{
+	Use:   "policy",
+	Short: "Create policy file, example: make policy user",
+	Run:   runMakePolicy,
 	Args:  cobra.ExactArgs(1), // 只允许且必须传 1 个参数
 }
 
-func runMakeAPI(cmd *cobra.Command, args []string) {
-	savePath := "app/controllers/api"
+func runMakePolicy(cmd *cobra.Command, args []string) {
+
+	savePath := "app/policy"
 	fileName := args[0]
 
 	if strings.Contains(args[0], "/") {
@@ -32,9 +33,11 @@ func runMakeAPI(cmd *cobra.Command, args []string) {
 
 	model := makeModelFromString(fileName)
 
-	// 组建目标目录
-	filePath := fmt.Sprintf("%s/%s.go", savePath, model.TableName)
+	fmt.Println(model)
+
+	// 拼接目标文件路径
+	filePath := fmt.Sprintf("app/policy/%s_policy.go", model.PackageName)
 
 	// 基于模板创建文件（做好变量替换）
-	createFileFromStub(filePath, "api", model)
+	createFileFromStub(filePath, "policy", model)
 }
